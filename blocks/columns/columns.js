a30fb34553ed5c13bf9813a612f16ecaa77334d9
Usman Khalid <usman.khalid@live.ca>
2025-09-18 11:17:19 -0400

    feat: basic columns stuff

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,9 +2,10 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
+    const columns = [...row.children];
+
+    columns.forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
